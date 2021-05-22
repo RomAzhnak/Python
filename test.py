4f2a5e4dd65a4f4c76dfb14023e057ed83230fe8
RomAzhnak <ars-125@yandex.ru>
2021-05-23 00:42:36 +0300

    One more editing

--- test.py
+++ test.py
@@ -1,28 +1,19 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#    print_hi('PyCharm')
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# if __name__ == '__main__':
 
-print(hash('a') & 7)
+
+import collections
+
 
 def print_hi(name):
     print(f'Hi, {name}')
 
-import collections
+
+print(hash('a') & 7)
+
 d = collections.deque()
-for i in range(3, 33): d.append('e%d' % i)  # добавить e3 - e32
+for i in range(3, 33):
+    d.append('e%d' % i)  # добавить e3 - e32
 print(d)
 print(d[0])
 
