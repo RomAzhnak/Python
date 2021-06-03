3f419c8311050acf3e3f1ca3ef75a9126cfcdcf0
RomAzhnak <ars-125@yandex.ru>
2021-06-03 11:36:58 +0300

    Reworked the code

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
 from sys import stdin
 
 
@@ -137,15 +120,4 @@ class SquareMatrix(Matrix):
             return (self * self) ** (p // 2)
 
 
-# exec(stdin.read())
-print(hash('a') & 7)
-
-def print_hi(name):
-    print(f'Hi, {name}')
-
-import collections
-d = collections.deque()
-for i in range(3, 33): d.append('e%d' % i)  # добавить e3 - e32
-print(d)
-print(d[0])
-print_hi('Rom')
+exec(stdin.read())
